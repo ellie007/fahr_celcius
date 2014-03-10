42b9889d9aeccbd0d6cab0edbd895e24bd43f706
Eleanor Lee <ellie007@gmail.com>
2014-03-10 14:41:03 -0500

    added header and C to F conversion

--- fahrenheit-celsius.c
+++ fahrenheit-celsius.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
+void fToCGraphHeader(){
+
+  char f = 'F';
+  char c = 'C';
+
+  printf("%c\t%c\n", f, c);
+}
+
+void cToFGraphHeader(){
+
+  char ce = 'C';
+  char fa = 'F';
+
+  printf("%c\t%c\n", ce, fa);
+}
+
+
 /* print Fahrenheit-Celsius table
     for fahr = 0, 20, ..., 300 */
-main()
+void fToCelcius()
 {
   float fahr, celsius;
   float lower, upper, step;
@@ -14,8 +31,35 @@ main()
   fahr = lower;
 
   while (fahr <= upper) {
-    celsius = 5 * (fahr - 32) / 9;
+    celsius = 5.0/9.0 * (fahr - 32.0);
     printf("%3.0f\t%6.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
+}
+
+void cToFarenheit()
+{
+  float farenheit, cels;
+  int bottom, ceiling, forwarder;
+
+  bottom = -20;
+  ceiling = 160;
+  forwarder = 20;
+
+  cels = bottom;
+
+  while (cels <= ceiling){
+    farenheit = cels * 9.0/5.0 + 32.0;
+    printf("%6.0f\t%3.0f\n", cels, farenheit);
+    cels = cels + forwarder;
+  }
+
+}
+
+int main(){
+  fToCGraphHeader();
+  fToCelcius();
+
+  cToFGraphHeader();
+  cToFarenheit();
 }
