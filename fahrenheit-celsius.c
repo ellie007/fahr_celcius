f84883314ed5e7997f44badc6752f1386fbdd6dc
Eleanor Lee <ellie007@gmail.com>
2014-03-10 15:25:39 -0500

    added symbolic constant names

--- fahrenheit-celsius.c
+++ fahrenheit-celsius.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 void fToCGraphHeader(){
 
   char f = 'F';
@@ -23,7 +27,7 @@ void fToCelcius()
 {
   int fahr;
 
-  for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
     printf("%3d\t%6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
   }
 }
